6361a403e8382b91340c5f78096e25bb947206fd
[dyad] <stsamueletrombetta@gmail.com>
2026-03-10 17:42:19 +0100

    [dyad] Rimosso codice specifico di Dyad - wrote 2 file(s)

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    if (process.env.NODE_ENV === "development") {
-      config.module.rules.push({
-        test: /\.(jsx|tsx)$/,
-        exclude: /node_modules/,
-        enforce: "pre",
-        use: "@dyad-sh/nextjs-webpack-component-tagger",
-      });
-    }
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
