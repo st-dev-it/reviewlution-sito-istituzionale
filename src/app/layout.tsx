ddbae5794dfbd7c69a0fa9e20d37b633d5d3e235
[dyad] <stsamueletrombetta@gmail.com>
2026-03-15 17:43:14 +0100

    [dyad] Abilitazione Vercel Analytics - wrote 3 file(s), added @vercel/analytics package(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
             <main className="flex-grow">{children}</main>
           </div>
           <Toaster />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
